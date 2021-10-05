cd54caf3eda76efd1fea4247a9b0026048ee9f77
scvry <animepsih666@gmail.com>
2021-10-05 14:49:01 +0200

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        int a;
-        a=400;
-        int abobus;
-        abobus=666;
-        int ak = abobus-a;
+        int abs;
     }
 }
